150f7928c0efdb477643c0b1abc8e9ec38ae5ab2
neamaAabdelzaher <142432776+neamaAabdelzaher@users.noreply.github.com>
2023-08-20 00:18:33 +0300

    Add files via upload

---
+++ lab1.php
@@ -0,0 +1,30 @@
+<?php
+// Q2:Write a program to print “Hello World” using echo
+
+use function PHPSTORM_META\type;
+
+echo "Hello Word <br>";
+
+// Q3:3.	Write a program to print 2 php variables using single echo statement.
+
+$first_name="Neama";
+$second_name="Ahmed";
+echo "hello, my name is :" .$first_name ." " .$second_name;
+
+//Bonus:difference between echo & print statement:
+//1-echo can take more than one variable or statement But print take only one varaible or one statment at a time .
+//2- echo is faster than print and used more.
+//3- echo returns no value (void) but print returns 1
+
+
+
+
+
+
+
+
+
+
+
+
+?>
